2fd17675f0a3bde86d3747257b365dd1d263b9e5
CaioTomas <caiotomas6@gmail.com>
2023-11-29 23:38:31 -0300

    comentando e alterando figuras

--- Report/Codes/crank-nicolson.py
+++ Report/Codes/crank-nicolson.py
@@ -2,48 +2,54 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-# Parameters
-k = 6.3  # Thermal diffusivity (m^2/year)
-L = 15.0  # Length of the rod (m)
-T = 1.0  # Total time (years)
-Nx = 150  # Number of spatial points
-Nt = 1260 #200  # Number of time steps
-dx = L / Nx  # Spatial step size
-dt = T / Nt  # Time step size
+plt.rcParams['text.usetex'] = True
 
-# Initial condition function
+# parâmetros
+k = 6.3  # difusividade térmica (m^2/year)
+L = 15.0  # profundidade máxima na integração
+T = 1.0  # tempo total de integração (em anos)
+Nx = 150  # quantidade de pontos espaciais
+Nt = 1260 #200  # quantidade de timesteps
+dx = L / Nx
+dt = T / Nt
+
+# condição inicial (começando com inverno)
 def f(t):
-    if (0 <= t <= 0.5) or (1 < t <= 1.5):
+    if (0 <= t <= 0.5):
         f = 1
-    elif (0.5 < t <= 1) or (1.5 < t <= 2):
+    elif (0.5 < t <= 1):
         f = 10
     return f
 
-# Boundary condition functions
+# condições de contorno
 def initial_condition(t, x):
     return f(t) * np.exp(-0.71 * x)
 
 def right_boundary_condition(t, x):
     return np.zeros_like(x)
 
-# Create grid
-x_values = np.linspace(0, L, Nx+1)  # Spatial grid
-u = initial_condition(0, x_values)  # Initial temperature distribution
+# criação da malha espacial
+x_values = np.linspace(0, L, Nx+1)
 
-frames = []  # To store frames for animation
+u = initial_condition(0, x_values)
+
+frames = []
+
+specific_times = [0, 0.25, 0.5, 0.75, 1.0]
+specific_frames = []
 
 alpha = k * dt / (2 * dx**2)
 
-# Build tridiagonal matrix coefficients
+# contruindo matriz (tridiagonal) dos coeficientes
 A = np.diag(1 + 2 * alpha * np.ones(Nx-1)) + \
     np.diag(-alpha * np.ones(Nx-2), k=1) + \
     np.diag(-alpha * np.ones(Nx-2), k=-1)
 
-# Crank-Nicolson
+# aplicando o método de Crank-Nicolson
 for n in range(Nt):
     u_new = np.zeros_like(u)
     
-    # Applying boundary conditions
+    # aplicando as condições de contorno
     u_new[0] = f((n+1) * dt)
     u_new[-1] = right_boundary_condition((n+1) * dt, x_values)[-1]
 
@@ -56,34 +62,53 @@ for n in range(Nt):
     u_new[1:Nx] = np.linalg.solve(A, b)
 
     u = u_new.copy()
+    frames.append(u.copy())
     
-    frames.append(u.copy())  # Store current temperature distribution for each frame
+    if (n * dt) in specific_times:
+        print(n*dt)
+        specific_frames.append(u.copy())
 
-# Create an animation
+specific_frames.append(u.copy())
+
+# criando a animação
 fig, ax = plt.subplots()
-line, = ax.plot(x_values, frames[0], label='Temperature distribution')
-plt.title('Temperature distribution over 1D rod')
-plt.xlabel('Position')
-plt.ylabel('Temperature')
+line, = ax.plot(x_values, frames[0], label='')
+plt.title('Perfil de temperatura')
+plt.xlabel('Profundidade')
+plt.ylabel('Temperatura')
 plt.legend()
 plt.grid(True)
 
-# Set y-axis range
 ax.set_ylim(0, 10)
 
-# Vertical line at x = 4.4
-ax.axvline(x=4.4, color='red', linestyle='--', label='x = 4.4')
+ax.axvline(x=4.4, color='red', linestyle='--', label='')
+ax.axhline(y=1, color='green', linestyle='--', label='')
 
-# Timer text
 timer_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
 
 def update(frame):
     line.set_ydata(frames[frame])
     time = (frame + 1) * dt  # Calculate current time
-    timer_text.set_text(f'Time: {time:.2f} years')  # Update timer text
+    timer_text.set_text(f't = {time:.2f} anos')
     return line, timer_text
 
 ani = FuncAnimation(fig, update, frames=len(frames), interval=50)
 
-# Save the animation as an MP4 file
-ani.save('temperature_evolution_crank_nicolson.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+ani.save('crank-nicolson.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+
+# criando a figura
+plt.figure(figsize=(8, 6))
+
+for i, time in enumerate(specific_times):
+    plt.plot(x_values, specific_frames[i], label=f't = {time} anos')
+
+plt.axvline(x=4.4, color='red', linestyle='--', label='')
+plt.axhline(y=1, color='green', linestyle='--', label='')
+
+plt.title('Perfil de temperatura')
+plt.xlabel('Profundidade')
+plt.ylabel('Temperatura')
+plt.legend()
+plt.grid(True)
+
+plt.savefig('crank-nicolson.pdf', bbox_inches='tight')
--- Report/Codes/exp-euler.py
+++ Report/Codes/exp-euler.py
@@ -2,77 +2,101 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-# Parameters
-k = 6.3  # Thermal diffusivity (m^2/year)
-L = 15.0  # Length of the rod (m)
-T = 1.0  # Total time (years)
-Nx = 150  # Number of spatial points
-Nt = 1260 #200  # Number of time steps
-dx = L / Nx  # Spatial step size
-dt = T / Nt  # Time step size
+plt.rcParams['text.usetex'] = True
 
-# Initial condition function
+# parâmetros
+k = 6.3  # difusividade térmica (m^2/year)
+L = 15.0  # profundidade máxima na integração
+T = 1.0  # tempo total de integração (em anos)
+Nx = 150  # quantidade de pontos espaciais
+Nt = 1260 #200  # quantidade de timesteps
+dx = L / Nx
+dt = T / Nt
+
+# condição inicial (começando com inverno)
 def f(t):
-    if (0 <= t <= 0.5) or (1 < t <= 1.5):
+    if (0 <= t <= 0.5):
         f = 1
-    elif (0.5 < t <= 1) or (1.5 < t <= 2):
+    elif (0.5 < t <= 1):
         f = 10
     return f
 
-# Boundary condition functions
+# condições de contorno
 def initial_condition(t, x):
     return f(t) * np.exp(-0.71 * x)
 
 def right_boundary_condition(t, x):
     return np.zeros_like(x)
 
-# Create grid
-x_values = np.linspace(0, L, Nx+1)  # Spatial grid
-u = initial_condition(0, x_values)  # Initial temperature distribution
+# criação da malha espacial
+x_values = np.linspace(0, L, Nx+1)
 
-frames = []  # To store frames for animation
+u = initial_condition(0, x_values)
 
-# Explicit Euler method
+frames = []
+
+specific_times = [0, 0.25, 0.5, 0.75, 1.0]
+specific_frames = []
+
+# aplicando o esquema de Euler explícito
 for n in range(Nt):
     u_new = np.zeros_like(u)
     
-    # Applying boundary conditions
+    # aplicando as condições de contorno
     u_new[0] = f((n+1) * dt)
     u_new[-1] = right_boundary_condition((n+1) * dt, x_values)[-1]
     
-    for i in range(1, Nx):  # Update inner points
+    for i in range(1, Nx):
         u_new[i] = u[i] + k * dt * (u[i+1] - 2 * u[i] + u[i-1]) / (dx**2)
     
     u = u_new.copy()
-    frames.append(u.copy())  # Store current temperature distribution for each frame
+    frames.append(u.copy())
     
-# Create an animation
+    if (n * dt) in specific_times:
+        print(n*dt)
+        specific_frames.append(u.copy())
+
+specific_frames.append(u.copy())
+
+# criando a animação
 fig, ax = plt.subplots()
-line, = ax.plot(x_values, frames[0], label='Temperature distribution')
-plt.title('Temperature distribution over 1D rod')
-plt.xlabel('Position')
-plt.ylabel('Temperature')
+line, = ax.plot(x_values, frames[0], label='')
+plt.title('Perfil de temperatura')
+plt.xlabel('Profundidade')
+plt.ylabel('Temperatura')
 plt.legend()
 plt.grid(True)
 
-# Set y-axis range
 ax.set_ylim(0,10)
 
-# Vertical line at x = 4.4
 ax.axvline(x=4.4, color='red', linestyle='--', label='x = 4.4')
+ax.axhline(y=1, color='green', linestyle='--', label='y = 1')
 
-# Timer text
 timer_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
 
 def update(frame):
     line.set_ydata(frames[frame])
     time = (frame + 1) * dt  # Calculate current time
-    timer_text.set_text(f'Time: {time:.2f} years')  # Update timer text
+    timer_text.set_text(f't = {time:.2f} anos')
     return line, timer_text
 
 ani = FuncAnimation(fig, update, frames=len(frames), interval=50)
 
-# Save the animation as an MP4 file
-ani.save('temperature_evolution.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+ani.save('exp-euler.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
 
-# plt.show()
+# criando a figura
+plt.figure(figsize=(8, 6))
+
+for i, time in enumerate(specific_times):
+    plt.plot(x_values, specific_frames[i], label=f't = {time} anos')
+
+plt.axvline(x=4.4, color='red', linestyle='--', label='')
+plt.axhline(y=1, color='green', linestyle='--', label='')
+
+plt.title('Perfil de temperatura')
+plt.xlabel('Profundidade')
+plt.ylabel('Temperatura')
+plt.legend()
+plt.grid(True)
+
+plt.savefig('exp-euler.pdf', bbox_inches='tight')
